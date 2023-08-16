7f37ef89989918bdf8af8c2b61fd49f3c9d9c046
Yaxin Hou <67990279+yaxinhou@users.noreply.github.com>
2023-08-16 09:21:09 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=25247804&pid=5331eac1094d6a4e207fa1368155df7e&tid=2d479b77d0f5064a16be283b7328386c",
+    "https://www.maimemo.com/share/page?uid=25247804&pid=9b7d6cfb587c326207ef06dddc5953b9&tid=552f37f6dfbadc3b7ad0a2f364d9be5a",
     "https://www.maimemo.com/share/page?uid=25247804&pid=a3c1365681c503158ca8342a3f28b5c8&tid=26caae158de3a0e3269259f49e29adbd",
     "https://www.maimemo.com/share/page?uid=25247804&pid=f251cd40cf2fcc9622eb60e2446973d7&tid=aee7dfb1e3708edab95cfbe16dfe0490",
   ]
