fcbb7bcb7545861e52119fae7ed21468457840f1
Yaxin Hou <67990279+yaxinhou@users.noreply.github.com>
2023-09-10 09:14:27 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     "https://www.maimemo.com/share/page?uid=25247804&pid=d9a5faf21865164ffcb656a850b149f7&tid=b53bf6007433cd1f0effe2bc15db6890",
-    "https://www.maimemo.com/share/page?uid=25247804&pid=ae9385f1cae9a26827fcf0f0194d52f8&tid=09390257879368f3a7b669f4c25f04cf",
+    "https://www.maimemo.com/share/page?uid=25247804&pid=8fa8614d127e79aea3a8305f92110ec6&tid=f43982798419afb185d2430d5a5f9709",
   ]
   /**
    * 生成随机数字
